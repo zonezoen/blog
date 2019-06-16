9df932fbb8256f1a97bb42800bf412a0acc6864f
zone <842221695@qq.com>
2019-06-16 22:59:46 +0800

    [add] 新添加了 Python logging 模块的使用文章与代码

--- python/logging模块/code/test_log.py
+++ python/logging模块/code/test_log.py
@@ -28,17 +28,78 @@ __date__ = '2019/6/16 下午3:23'
 # logger.warning("Log level warning")
 
 
+#
+# import logging
+#
+# logger = logging.getLogger(__name__)
+# logger.setLevel(level=logging.INFO)
+# handler = logging.FileHandler("log.txt")
+# handler.setLevel(logging.INFO)
+# formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+# handler.setFormatter(formatter)
+# logger.addHandler(handler)
+#
+# logger.info("Log level info")
+# logger.debug("Log level debug")
+# logger.warning("Log level warning")
 
-import logging
 
-logger = logging.getLogger(__name__)
-logger.setLevel(level=logging.INFO)
-handler = logging.FileHandler("log.txt")
-handler.setLevel(logging.INFO)
-formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
-handler.setFormatter(formatter)
-logger.addHandler(handler)
 
-logger.info("Log level info")
-logger.debug("Log level debug")
-logger.warning("Log level warning")
+# import json
+# import logging.config
+# import os
+#
+#
+# def setup_logging(default_path="log_cfg.json", default_level=logging.INFO, env_key="LOG_CFG"):
+#     path = default_path
+#     value = os.getenv(env_key, None)
+#     if value:
+#         path = value
+#     if os.path.exists(path):
+#         with open(path, "r") as f:
+#             config = json.load(f)
+#             logging.config.dictConfig(config)
+#     else:
+#         logging.basicConfig(level=default_level)
+#
+#
+# def func():
+#     logging.info("start func")
+#
+#     logging.info("exec func")
+#
+#     logging.info("end func")
+#
+#
+# if __name__ == "__main__":
+#     setup_logging(default_path="log_cfg.json")
+#     func()
+
+
+
+import yaml
+import logging.config
+import os
+
+def setup_logging(default_path = "logging.yaml",default_level = logging.INFO,env_key = "LOG_CFG"):
+    path = default_path
+    value = os.getenv(env_key,None)
+    if value:
+        path = value
+    if os.path.exists(path):
+        with open(path,"r") as f:
+            config = yaml.load(f)
+            logging.config.dictConfig(config)
+    else:
+        logging.basicConfig(level = default_level)
+
+def func():
+    logging.info("start func")
+
+    logging.info("exec func")
+
+    logging.info("end func")
+
+if __name__ == "__main__":
+    setup_logging(default_path = "log_cfg.yaml")
+    func()
