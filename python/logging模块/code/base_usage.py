11e0cd4b841e47fde16f24a7120b40c7d63f9177
lzr <842221695@qq.com>
2019-06-17 14:10:56 +0800

    [add] 完善代码

---
+++ python/logging模块/code/base_usage.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+__auth__ = 'zone'
+__date__ = '2019/6/16 下午3:23'
+'''
+公众号：zone7
+小程序：编程面试题库
+'''
+
+import logging
+import sys
+
+try:
+    level_msg = sys.argv[1]
+    if level_msg == "--debug":
+        log_level = logging.DEBUG
+    else:
+        log_level = logging.INFO
+except Exception as e:
+    log_level = logging.DEBUG
+log_level = logging.DEBUG
+
+
+logging.basicConfig(level=log_level, format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+logger = logging.getLogger(__name__)
+
+logger.info("Log level info")
+logger.debug("Log level debug")
+logger.warning("Log level warning")
